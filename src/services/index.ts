31bd5e18234dcdf083cd159bd14491be7428d42e
lucashegomes <lucashegomes@gmail.com>
2026-03-06 15:01:38 -0300

    feat: add centralized services exports

---
+++ src/services/index.ts
@@ -0,0 +1,11 @@
+import { studentService } from "./api/studentService";
+import { teacherService } from "./api/teacherService";
+import { courseService } from "./api/courseService";
+import { classService } from "./api/classService";
+import { checkinService } from "./api/checkinService";
+
+export const studentsService = studentService;
+export const teachersService = teacherService;
+export const coursesService = courseService;
+export const classesService = classService;
+export const checkinsService = checkinService;
